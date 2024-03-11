1a3c6d1cf40b62e52b9d941ac4cade7494540ffd
Hetzner CAX11 <ajayvignesh01@gmail.com>
2024-03-11 22:10:28 +0000

    pretty much

--- deps.ts
+++
@@ -1,2 +0,0 @@
-import "https://deno.land/x/jose@v4.14.1/index.ts"
-import "https://deno.land/std@0.131.0/http/server.ts"
--- upgrade.sh
+++ upgrade.sh
@@ -1,5 +1,5 @@
 reload_nginx() {  
-  docker exec nginx /usr/sbin/nginx -s reload  
+  docker exec nginx /usr/sbin/nginx -s reload # -c /etc/nginx/nginx.conf -T
 }
 
 zero_downtime_deploy() {
@@ -14,19 +14,22 @@ zero_downtime_deploy() {
   new_container_ip=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $new_container_id)
   curl --retry-connrefused --retry 10 --retry-delay 1 --fail $new_container_ip:9000/_internal/health || exit 1
 
-  # update nginx.conf with the new container IP or hostname
-  sed -i "s/web/$new_container_ip/g" nginx.conf
+  # # update nginx.conf with the new container IP or hostname
+  # # Changes reflected in docker volume, but running `docker exec nginx nginx -T` shows reload still taking old conf
+  # sed -i "s/web/$new_container_ip/g" nginx.conf
 
   # start routing requests to the new container
+  # AND old container until we fix reload not taking new conf
   reload_nginx
 
-  # take the old container offline (handle SIGTERM or set -t according to your needs)
-  docker stop $old_container_id
+  # take the old container offline (handle SIGTERM and set timeout accordingly)
+  docker stop $old_container_id --timeout 10
   docker rm $old_container_id
   docker compose up -d --no-deps --scale $service_name=1 --no-recreate $service_name
 
-  # reset nginx config
-  sed -i "s/$new_container_ip/web/g" nginx.conf
+  # # reset nginx config
+  # # Uncomment after fixing reload not taking new conf
+  # sed -i "s/$new_container_ip/web/g" nginx.conf
 
   # stop routing requests to the old container  
   reload_nginx
